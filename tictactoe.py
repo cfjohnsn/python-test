ea225e2868690c1f2f66024c3b7e95a819053900
cfjohnsn <johnsonfconnor0603@gmail.com>
2021-02-24 11:32:16 -0800

    More Tictactoe stuff

--- tictactoe.py
+++ tictactoe.py
@@ -2,15 +2,15 @@
 
 
 def printboard():
-    boardSize = 3
-    x = 1
-    while x <= boardSize:
-        print (" ---" *boardSize + "\n" + "|   " *(boardSize+1))
-        x = x+1
-    print (' ---' * boardSize)
+    boardSize = 3           #Variable decides how big the board is, could be made bigger or let user input.
+    x = 1                   #starting point
+    while x <= boardSize:   #Helps the program know when to stop
+        print (" ---" *boardSize + "\n" + "|   " *(boardSize+1))    #Creates a row in the board.
+        x = x+1                                                     #Helps the program know when to stop.
+    print (' ---' * boardSize)   #Adds the last row
 
 
-printboard()
+printboard()    #Calls the function
 
 
 
