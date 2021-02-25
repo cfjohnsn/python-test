6a64d86d325f3a940555a334e71b44373554716c
cfjohnsn <johnsonfconnor0603@gmail.com>
2021-02-24 19:22:35 -0800

    moving

--- tictactwo.py
+++ tictactwo.py
@@ -5,10 +5,10 @@ gameboard = [[0, 0, 0],[0, 0, 0,], [0, 0, 0]]   #List of lists!
 
 def show_game(player=0, x=0, y=0, just_display = False):                 #function!
     print ('   0  1  2')
-    if not just_display:                        #Makes sure the player put an input in
-        gameboard [x][y] = player               #Changes gameboard to add what the player put in.
-    for count, row in enumerate(gameboard):     #row is just each item in the list, in this example, each list.
-        print (count, row)                      #Printing the row is just printing the item on the list. 
+    if not just_display:                                  #Makes sure the player put an input in
+        gameboard [x][y] = player                           #Changes gameboard to add what the player put in.
+    for count, row in enumerate(gameboard):                 #row is just each item in the list, in this example, each list.
+        print (count, row)                                  #Printing the row is just printing the item on the list. 
     
 
 def p1make_move():
